45e8aa8d87aec52b4a37e4b912f442e9726a49c5
우수연 <woosean999@gmail.com>
2023-09-11 23:22:00 +0900

    [particles] make Canvas Class extends CanvasOption

--- boilerplate/js/index.js
+++ boilerplate/js/index.js
@@ -1,43 +1,48 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-const dpr = window.devicePixelRatio;
-let fps = 60;
-const interval = 1000 / fps;
-let now, delta;
-let then = Date.now();
+import CanvasOption from "./CanvasOption.js";
 
-let canvasWidth, canvasHeight;
-
-function init() {
-  canvasWidth = innerWidth;
-  canvasHeight = innerHeight;
-
-  canvas.width = canvasWidth * dpr;
-  canvas.height = canvasHeight * dpr;
-  ctx.scale(dpr, dpr);
-
-  canvas.style.width = `${canvasWidth}px`;
-  canvas.style.height = `${canvasHeight}px`;
-}
-
-function render() {
-  window.requestAnimationFrame(render);
-  now = Date.now();
-  delta = now - then;
-  if (delta < interval) {
-    return;
+class Canvas extends CanvasOption {
+  constructor() {
+    super();
   }
 
-  ctx.fillRect(100, 100, 200, 200);
+  init() {
+    this.canvasWidth = innerWidth;
+    this.canvasHeight = innerHeight;
+    this.canvas.width = this.canvasWidth * this.dpr;
+    this.canvas.height = this.canvasHeight * this.dpr;
+    this.ctx.scale(this.dpr, this.dpr);
 
-  then = now - (delta % interval);
+    this.canvas.style.width = `${this.canvasWidth}px`;
+    this.canvas.style.height = `${this.canvasHeight}px`;
+  }
+
+  render() {
+    let now, delta;
+    let then = Date.now();
+
+    const frame = () => {
+      window.requestAnimationFrame(frame);
+      now = Date.now();
+      delta = now - then;
+      if (delta < this.interval) {
+        return;
+      }
+
+      this.ctx.fillRect(100, 100, 200, 200);
+
+      then = now - (delta % this.interval);
+    };
+    requestAnimationFrame(frame);
+  }
 }
 
+const canvas = new Canvas();
+
 window.addEventListener("load", () => {
-  init();
-  render();
+  canvas.init();
+  canvas.render();
 });
 
 window.addEventListener("resize", () => {
-  init();
+  canvas.init();
 });
