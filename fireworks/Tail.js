66918caecddc80f1de9db9460064ee1141606ce3
우수연 <woosean999@gmail.com>
2023-09-29 18:26:46 +0900

    [particles] fireworks: twisting tails

--- fireworks/Tail.js
+++ fireworks/Tail.js
@@ -1,9 +1,11 @@
+import {randomNumBetween} from "../utils.js";
 import CanvasOption from "./CanvasOption.js";
 
 export default class Tail extends CanvasOption {
   constructor(x, vy, color) {
     super();
     this.x = x;
+    this.angle = randomNumBetween(0, 2);
     this.y = this.canvasHeight;
     this.vy = vy;
     this.color = color;
@@ -13,6 +15,10 @@ export default class Tail extends CanvasOption {
   update() {
     this.vy *= this.friction;
     this.y += this.vy;
+
+    this.angle += 1;
+    this.x += Math.cos(this.angle) * this.vy * 0.2;
+
     this.opacity = -this.vy * 0.1;
   }
 
